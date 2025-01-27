83cc6a78af349225b91a3088907d257b9bd42441
Benny Bastin Roy <benny.roy@chryselys.com>
2025-01-27 17:12:29 +0530

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -10,23 +10,20 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Sales-Force App',
+      title: 'Sales-force-app',
       theme: ThemeData(
         scaffoldBackgroundColor: Colors.white,
         canvasColor: Colors.white,
       ),
       home: LayoutBuilder(
         builder: (context, constraints) {
-          // Restrict the app width and height to simulate a mobile screen size
+          // Restrict the app width to simulate a mobile screen size
           return Center(
             child: ConstrainedBox(
               constraints: BoxConstraints(
-                maxWidth: 400,  // Mobile screen width
+                maxWidth: 450, // Mobile screen width
               ),
-              child: AspectRatio(
-                aspectRatio: 400 / 800, // Ensure the aspect ratio is correct
-                child: BottomNavBarScreen(),
-              ),
+              child: BottomNavBarScreen(),
             ),
           );
         },
